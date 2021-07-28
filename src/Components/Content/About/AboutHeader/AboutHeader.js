fe6afcdd988dacff9c1f8d1e5ff2d8fc72f4e82f
Anton <urt.sidorenko@yandex.ru>
2021-07-28 13:47:05 +0300

    added material-ui fonts and button on AboutHeader

--- src/Components/Content/About/AboutHeader/AboutHeader.js
+++ src/Components/Content/About/AboutHeader/AboutHeader.js
@@ -1,5 +1,7 @@
+
 import React from 'react'
 import s from './AboutHeader.module.css'
+import { Button } from '@material-ui/core'
 
 const AboutHeader = () => {
 
@@ -9,13 +11,13 @@ const AboutHeader = () => {
 			<div className={s.content}>
 				<div className={s.content__item}>
 					<h1>Проектирование и строительство объектов связи любой сложности</h1>
-					<h3>на рынке более 20 лет</h3>
-					<h3>на рынке более 20 лет</h3>
-					<h3>на рынке более 20 лет</h3>
-					<h3>на рынке более 20 лет</h3>
+					<h3>более 20 лет на рынке</h3>
+					<Button className={s.content__button} variant='outlined' size='large' color='primary'>
+						подробнее
+					</Button>
 				</div>
 			</div>
-		</div>
+		</div >
 	)
 }
 
