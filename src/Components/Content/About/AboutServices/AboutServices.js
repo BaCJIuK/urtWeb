584788687b3aaaae1b56c5fd42515d9e98f135ea
Anton <urt.sidorenko@yandex.ru>
2021-08-05 17:48:34 +0300

    working on AboutServices block

--- src/Components/Content/About/AboutServices/AboutServices.js
+++ src/Components/Content/About/AboutServices/AboutServices.js
@@ -1,32 +1,50 @@
 import React from 'react'
 import s from './AboutServices.module.css'
 import buildFiber from '../../../../img/aboutServices/buildFiber.jpg'
+import mobileNetwork from '../../../../img/aboutServices/mobileNetwork2.jpg'
+import gnb from '../../../../img/aboutServices/gnb1.jpg'
 
 
 const AboutServices = () => {
 	return (
 		<div className={s.AboutServices}>
-			<div className={`${s.AboutServices__item} ${s.servicesItem}`}>
+			<div className={`${s.AboutServices__item} ${s.servicesItem}`} data-aos='fade-left'>
 				<div className={s.servicesItem__image}><img src={buildFiber} /></div>
 				<div className={`${s.servicesItem__content} ${s.content}`}>
 					<div className={s.content__title}><h3>Строительство ВОЛС</h3></div>
-					<p className={s.content__text}></p>
+					<p className={s.content__text}>За последние 5 лет нами построено более 1000 км ВОЛС. Для строительства ВОЛС в ООО «Южрегионтелеком» организованы две механизированные колонны, оснащенные всей необходимой современной техникой (кабелеукладчики, землеройная техника,
+					установки горизонтально-направленного бурения ГНБ и лаборатории для измерений и монтажа волоконно-оптических кабелей).</p>
 				</div>
 			</div>
-			<div className={`${s.AboutServices__item} ${s.servicesItem}`}>
-				<div className={s.servicesItem__text}></div>
-				<div className={s.servicesItem__image}></div>
 
-			</div>
-			<div className={`${s.AboutServices__item} ${s.servicesItem}`}>
-				<div className={s.servicesItem__image}></div>
-				<div className={s.servicesItem__text}></div>
-			</div>
-			<div className={`${s.AboutServices__item} ${s.servicesItem}`}>
-				<div className={s.servicesItem__text}></div>
-				<div className={s.servicesItem__image}></div>
 
+			<div className={`${s.AboutServices__item} ${s.servicesItem}`} data-aos='fade-right'>
+				<div className={`${s.servicesItem__content} ${s.content}`}>
+					<div className={s.content__title}><h3>Строительство объектов мобильной связи</h3></div>
+					<p className={s.content__text}>Специалистами нашей компании смонтировано и настроено более 3000 базовых станций и более 100 коммутаторов мобильной связи в России и странах СНГ.
+					Вместе с монтажом оборудования базовых станций и коммутаторов производится монтаж систем электропитания Siemens KS600, Power One, УЭПС-2,3 . Так же мы выполняем работы по строительству и настройке антенно-фидерных трактов любой сложности и модификации..</p>
+				</div>
+				<div className={s.servicesItem__image}><img src={mobileNetwork} /></div>
 			</div>
+
+			<div className={`${s.AboutServices__item} ${s.servicesItem}`} data-aos='fade-left'>
+				<div className={s.servicesItem__image}><img src={gnb} /></div>
+				<div className={`${s.servicesItem__content} ${s.content}`}>
+					<div className={s.content__title}><h3>Строительство ВОЛС</h3></div>
+					<p className={s.content__text}>За последние 5 лет нами построено более 1000 км ВОЛС. Для строительства ВОЛС в ООО «Южрегионтелеком» организованы две механизированные колонны, оснащенные всей необходимой современной техникой (кабелеукладчики, землеройная техника,
+					установки горизонтально-направленного бурения ГНБ и лаборатории для измерений и монтажа волоконно-оптических кабелей).</p>
+				</div>
+			</div>
+
+			<div className={`${s.AboutServices__item} ${s.servicesItem}`} data-aos='fade-right'>
+				<div className={`${s.servicesItem__content} ${s.content}`}>
+					<div className={s.content__title}><h3>Строительство объектов мобильной связи</h3></div>
+					<p className={s.content__text}>Специалистами нашей компании смонтировано и настроено более 3000 базовых станций и более 100 коммутаторов мобильной связи в России и странах СНГ.
+					Вместе с монтажом оборудования базовых станций и коммутаторов производится монтаж систем электропитания Siemens KS600, Power One, УЭПС-2,3 . Так же мы выполняем работы по строительству и настройке антенно-фидерных трактов любой сложности и модификации..</p>
+				</div>
+				<div className={s.servicesItem__image}><img src={mobileNetwork} /></div>
+			</div>
+
 
 		</div>
 	)
